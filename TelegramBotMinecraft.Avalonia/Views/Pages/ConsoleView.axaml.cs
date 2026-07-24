c2d3fe0814d5ba178e2006b2294deba6bb1d4260
TRONMAX <popik.maxim@gmail.com>
2026-07-25 01:33:52 +0300

    feat: add auto-scroll to log

--- TelegramBotMinecraft.Avalonia/Views/Pages/ConsoleView.axaml.cs
+++ TelegramBotMinecraft.Avalonia/Views/Pages/ConsoleView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System;
 using TelegramBotMinecraft.Avalonia.ViewModels;
 
 namespace TelegramBotMinecraft.Avalonia;
@@ -9,9 +10,21 @@ public partial class ConsoleView : UserControl
     {
         InitializeComponent();
 
-        DataContext = new ConsoleViewModel(new MinecraftServerManager(), 
-            new Core.Database.ServerRepository(), 
+        DataContext = new ConsoleViewModel(new MinecraftServerManager(),
+            new Core.Database.ServerRepository(),
             new Core.Services.ServerStatusService(new Core.Database.ServerRepository()),
             new Core.Services.ServerLogService(new Core.Database.ServerRepository()));
+
+        var logConsole = this.FindControl<AvaloniaEdit.TextEditor>("LogConsole");
+        if (logConsole != null)
+        {
+            logConsole.Options.AllowScrollBelowDocument = false;
+            LogConsole.TextChanged += LogConsole_TextChanged;
+        }
+
+    }
+    private void LogConsole_TextChanged(object? sender, EventArgs e)
+    {
+        LogConsole.ScrollToLine(LogConsole.LineCount);
     }
 }
