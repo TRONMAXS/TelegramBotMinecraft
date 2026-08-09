a6750ee5eef8207c5fd8806625490510455d596e
TRONMAX <popik.maxim@gmail.com>
2026-08-09 23:35:42 +0300

    fix(app): prevent duplicate main window opening

--- TelegramBotMinecraft.Avalonia/Program.cs
+++ TelegramBotMinecraft.Avalonia/Program.cs
@@ -1,13 +1,28 @@
 ﻿using Avalonia;
 using System;
+using System.Threading;
 
 namespace TelegramBotMinecraft.Avalonia;
 
 class Program
 {
+    private static Mutex? _mutex;
+    private const string MutexName = "TRONMAXS.TelegramBotMinecraft.SingleInstanceMutex";
+
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        _mutex = new Mutex(true, MutexName, out bool isNewInstance);
+
+        if (!isNewInstance) return;
+
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        finally { _mutex.ReleaseMutex(); }
+
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
