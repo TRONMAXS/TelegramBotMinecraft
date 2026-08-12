2b71a3b5d7143a4772989a0d780adbb6bb20ae33
TRONMAX <popik.maxim@gmail.com>
2026-08-13 02:33:30 +0300

    feat(java): implement dynamic UI behavior for Java download and version list

--- TelegramBotMinecraft.Avalonia/ViewModels/JavaDownloadViewModel.cs
+++ TelegramBotMinecraft.Avalonia/ViewModels/JavaDownloadViewModel.cs
@@ -1,10 +1,12 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 using TelegramBotMinecraft.Core.Models;
 using TelegramBotMinecraft.Core.Services;
 
@@ -12,7 +14,8 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
 {
     public partial class JavaDownloadViewModel : ObservableObject
     {
-        private readonly JavaManagerService? _JavaManagerService;
+        private readonly JavaManagerService _JavaManagerService;
+        private JavaManagerWindowViewModel? _parent;
 
         public ObservableCollection<JavaManager>? JavaList { get; } = new();
         public ObservableCollection<JavaInfoDownload>? JavaListInfo { get; } = new();
@@ -21,11 +24,11 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
         private JavaManager? _selectedJava;
 
         [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsDownloadedEnabled))]
+        [NotifyCanExecuteChangedFor(nameof(DownloadCommand))]
+        [NotifyCanExecuteChangedFor(nameof(UpdateCommand))]
+        [NotifyCanExecuteChangedFor(nameof(CancelDownloadCommand))]
         private JavaInfoDownload? _selectedJavaInfo;
-
-
-        [ObservableProperty]
-        private bool? _isDownloading = false;
 
         [ObservableProperty]
         private string? _downloadProgressString;
@@ -36,16 +39,39 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
         [ObservableProperty]
         private string? _downloadFilesProgressString;
 
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsDownloadedEnabled))]
+        [NotifyCanExecuteChangedFor(nameof(DownloadCommand))]
+        [NotifyCanExecuteChangedFor(nameof(UpdateCommand))]
+        [NotifyCanExecuteChangedFor(nameof(CancelDownloadCommand))]
+        private bool _isDownloading;
+        public bool IsDownloadedEnabled => !IsDownloading;
+
+        
+
+
         private CancellationTokenSource? _cts;
 
-        public JavaDownloadViewModel(JavaManagerService? javaManagerService)
+        public JavaDownloadViewModel(JavaManagerService javaManagerService)
         {
             _JavaManagerService = javaManagerService;
-
             LoadJavaList();
         }
 
-        private async void LoadJavaList()
+        public void SetParent(JavaManagerWindowViewModel parent)
+        {
+            _parent = parent;
+            UpdateParentStatus();
+        }
+        private void UpdateParentStatus()
+        {
+            if (_parent != null)
+            {
+                _parent.IsDownloadedEnabled = this.IsDownloadedEnabled;
+            }
+        }
+
+        public async void LoadJavaList()
         { 
             SelectedJava = null;
             SelectedJavaInfo = null;
@@ -59,6 +85,10 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
             {
                 JavaList?.Add(new JavaManager($"Java {java.Version}", java.Architecture));
             }
+
+            DownloadProgressString = string.Empty;
+            DownloadFilesProgressString = string.Empty;
+            DownloadProgressValue = 0;
         }
 
         private async void LoadJavaInfoList()
@@ -78,18 +108,11 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
             }
         }
 
-        [RelayCommand]
+        [RelayCommand(CanExecute = nameof(CanUpload))]
         private async Task Download()
         {
             if (SelectedJava == null || SelectedJavaInfo == null) return;
             if (JavaList == null || JavaListInfo == null) return;
-
-            IsDownloading = true;
-            bool isSuccess = false;
-            DownloadProgressValue = 0;
-            int countDownloadedFiles = 0;
-            _cts = new CancellationTokenSource();
-
 
 
             DownloadProgressString = "Проверка...";
@@ -104,76 +127,108 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
             }
 
 
+            IsDownloading = true;
+            bool isSuccess = false;
+            DownloadProgressValue = 0;
+            int countDownloadedFiles = 0;
+            _cts = new CancellationTokenSource();
+
             DownloadProgressString = "Подготовка к скачиванию...";
             DownloadFilesProgressString = "Файлы: 0/0";
 
             try
             {
-                await foreach (var currentProgress in _JavaManagerService.JavaDownloader(SelectedJavaInfo.Name, _cts.Token))
+                await Task.Run(async () =>
                 {
-                    if (currentProgress.Progress == -1)
+                    if(SelectedJavaInfo == null) return;
+                    var downloader = _JavaManagerService.JavaDownloader(SelectedJavaInfo.Name, _cts.Token)
+                                    .ConfigureAwait(false);
+
+                    await foreach (var currentProgress in downloader)
                     {
-                        DownloadProgressString = "Ошибка: файлы повреждены или не скачались. Попробуйте снова.";
-                        DownloadProgressValue = 0;
-                        countDownloadedFiles = 0;
-                        isSuccess = false;
-                        break;
+                        if (currentProgress.Progress == -1)
+                        {
+                            await Dispatcher.UIThread.InvokeAsync(() =>
+                            {
+                                DownloadProgressString = "Ошибка: файлы повреждены или не скачались. Попробуйте снова.";
+                                DownloadProgressValue = 0;
+                                isSuccess = false;
+                            });
+                            break;
+                        }
+
+                        await Dispatcher.UIThread.InvokeAsync(() =>
+                        {
+                            DownloadProgressValue = currentProgress.Progress;
+                            int countDownloadedFiles = currentProgress.completedFiles;
+                            DownloadProgressString = $"Скачивание и установка Java... {currentProgress.Progress}%";
+                            DownloadFilesProgressString = $"Файлы: {countDownloadedFiles}/{currentProgress.AllFiles}";
+                        });
+
+                        if (currentProgress.Progress == 100) isSuccess = true;
                     }
-
-                    DownloadProgressValue = currentProgress.Progress;
-                    countDownloadedFiles = currentProgress.completedFiles;
-                    DownloadProgressString = $"Скачивание и установка Java... {currentProgress.Progress}%";
-                    DownloadFilesProgressString = $"Файлы: {countDownloadedFiles}/{currentProgress.AllFiles}";
-
-                    if (currentProgress.Progress == 100) isSuccess = true;
-                }
+                });
                 if (isSuccess)
                 {
                     DownloadProgressString = "Среда Java успешно установлена и проверена!";
                     DownloadFilesProgressString = "Готово!";
+                    await _JavaManagerService.UpdateJavaInDb();
                 }
             }
-            catch (OperationCanceledException) 
-            { 
+            catch (OperationCanceledException)
+            {
                 DownloadProgressString = "Загрузка успешно отменена.";
                 DownloadFilesProgressString = "Остановлено";
                 DownloadProgressValue = 0;
-
             }
             catch { DownloadProgressString = "Ошибка при установке Java компонентов."; }
-            finally 
-            { 
-                IsDownloading = false; 
-                _cts?.Dispose(); 
-                _cts = null; 
+            finally
+            {
+                IsDownloading = false;
+                _cts?.Dispose();
+                _cts = null;
             }
         }
 
-        [RelayCommand]
+        [RelayCommand(CanExecute = nameof(CanUpdate))]
         private void Update()
         {
             LoadJavaList();
             LoadJavaInfoList();
         }
 
-        [RelayCommand]
+        [RelayCommand(CanExecute = nameof(CanDownloading))]
         private void CancelDownload()
         {
+            if (!IsDownloading || _cts == null) return;
             _cts?.Cancel();
-
             DownloadProgressString = "Скачивание отменено пользователем.";
-            IsDownloading = false;
         }
+
+        private bool CanDownloading() => IsDownloading;
+
+        private bool CanUpload () => !IsDownloading && SelectedJavaInfo != null;
+
+        private bool CanUpdate () => !IsDownloading;
+
 
         partial void OnSelectedJavaChanged(JavaManager? value)
         {
             if (value == null) return;
             LoadJavaInfoList();
+            DownloadProgressString = string.Empty;
+            DownloadFilesProgressString = string.Empty;
+            DownloadProgressValue = 0;
         }
 
         partial void OnSelectedJavaInfoChanged(JavaInfoDownload? value)
         {
             if (value == null) return;
+        }
+
+        partial void OnIsDownloadingChanged(bool value)
+        {
+            UpdateParentStatus();
         }
     }
 }
