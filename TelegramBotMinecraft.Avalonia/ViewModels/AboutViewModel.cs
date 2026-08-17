ba66b4299c6e1392a48b2cec7ee2fb5ca1d5ef19
TRONMAX <popik.maxim@gmail.com>
2026-08-18 01:40:11 +0300

    feat(ui): implement dynamic application version loading in About window

--- TelegramBotMinecraft.Avalonia/ViewModels/AboutViewModel.cs
+++ TelegramBotMinecraft.Avalonia/ViewModels/AboutViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Reflection;
 
 namespace TelegramBotMinecraft.Avalonia.ViewModels
 {
@@ -8,7 +9,24 @@ namespace TelegramBotMinecraft.Avalonia.ViewModels
     {
         public Action? CloseAction { get; set; }
 
+        [ObservableProperty]
+        private string _versionProgramm;
 
+        public AboutViewModel()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            string? informationalVersion = Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                .InformationalVersion;
+
+            VersionProgramm = informationalVersion ?? version?.ToString() ?? "1.0.0";
+
+            if (VersionProgramm.Contains("+"))
+            {
+                VersionProgramm = VersionProgramm.Split('+')[0];
+            }
+        }
 
         [RelayCommand]
         public void OnClose() { CloseAction?.Invoke(); }
